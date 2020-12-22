5c5692ee31680637648de93b8292d422172e5288
Patrick Little <Patrick.Little@CompassES.com>
2020-12-22 14:50:52 -0600

    Updating deploy script based on improvements from other deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,64 @@
 #!/bin/bash
 
-#Push the modified manifest to the public manifest repo
+usage="Usage: ./deploy.sh <tag name to release> <manifest file>"
+
+#Push the modified manifest to the public manifest repo and tags boths repos with the tag
+# provided in the first argument
+
+#IMPORTANT
+#expecting to be run on products *-master branch and the script determines the destination
+#branch from that name. See the README.md in the beacon-manifests-private master branch 
+#for more information.
+
 #Expected to be in a repo with both the private and public manifest repos as remotes
 #git clone https://github.com/BeaconEmbeddedWorks/beacon-manifests-private.git
 #git remote add public https://github.com/BeaconEmbeddedWorks/beacon-manifests.git
 
+#confirm remotes
+originUrl=$(git config --get remote.origin.url)
+publicUrl=$(git config --get remote.public.url)
+if [ "$originUrl" != "https://github.com/BeaconEmbeddedWorks/beacon-manifests-private.git" ]
+then
+	echo Remote 'origin' did not match expectef
+	exit 1
+else
+	git fetch public
+fi
+if [ "$publicUrl" != "https://github.com/BeaconEmbeddedWorks/beacon-manifests.git" ]
+then
+	echo Remote 'public' did not match expected
+	exit 1
+else
+	git fetch public
+fi
+
+#Get input
+if [ -z "$1" ]
+then
+	echo argument 1 must be the tag to use
+	echo $usage
+	exit 1
+else
+	tag=$1
+fi
+if [ -z "$2" ]
+then
+	echo argument 2 must be the manifest filename
+	echo $usage
+	exit 1
+else
+	manifest=$2
+fi
+
 srcBranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 destBranch=$(git branch | sed -n -e 's/^\* \(.*\)-.*/\1/p')
-tag=$(git tag --points-at HEAD | grep -v '\-RC')
+tag=$1
 #debug 
-echo $srcBranch $destBranch $tag
+#echo $srcBranch $destBranch $tag
+
+#Tag the private repo
+git tag -f -a $tag-private -m "Deploying manifest for $tag"
+git push -f origin $tag-private
 
 #check if branch exists, or start from main
 git ls-remote --heads public $destBranch | wc -l
@@ -22,26 +71,30 @@ else
 	git checkout -B $destBranch public/main
 fi
 
-git checkout origin/$srcBranch -- imx8mm-4.19.xml
+#Overwrite the manifest 
+git checkout origin/$srcBranch -- $manifest
 
 #Edit manifest xml in place preserving formatting
 #Delete any remotes named 'third'
 #Delete any projects using the renote named 'third'
 #Remove the '-private' suffix from project repo names
 #Remove the '-RCx' suffix from project revisions
-#File to modify - could be parameterized
+#File to modify
+
+#COPYING TO OTHER PRODUCT
+#review this part of the script and update as appropriate
 
 xmlstarlet ed --inplace --pf \
 	-d "/manifest/remote[@name='third']" \
 	-d "/manifest/project[@remote='third']" \
 	-u "/manifest/project[contains(@name,'-private')]/@name" -x "substring-before(., '-private')" \
 	-u "/manifest/project[contains(@revision,'-RC')]/@revision" -x "substring-before(., '-RC')" \
-	imx8mm-4.19.xml
+	$manifest
 
-git add imx8mm-4.19.xml
+git add $manifest
 
 git commit -m "Deploying manifest for $tag"
 git tag -f -a $tag -m "Deploying manifest for $tag"
-git push -u public HEAD
+git push -u public $destBranch
 git push -f public $tag
 git checkout -
